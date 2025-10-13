5be121ff69105f5fd62b91363da94b473cef3e12
GoDisCover <jessenco122@gmail.com>
2025-10-13 22:29:53 +0700

    doneeeeeee

--- react_latian_uts/src/pages/quotes/Quotes.jsx
+++ react_latian_uts/src/pages/quotes/Quotes.jsx
@@ -1,6 +1,7 @@
 import http from "@/api/apiClient";
 import { useCallback, useEffect, useState } from "react"
 import Button from "../../components/ui/Button";
+import { NavLink } from "react-router-dom";
 
 export default function QuotePage() {
     const [isLoading, setIsLoading] = useState(false);
@@ -9,7 +10,7 @@ export default function QuotePage() {
     const fetchQuotes = useCallback(async () => {
         try {
             setIsLoading(true);
-            const response = await http.get("/quotes"); 
+            const response = await http.get("/quotes");
 
             setQuotes(response.data.data);
         } catch (error) {
@@ -18,6 +19,20 @@ export default function QuotePage() {
             setIsLoading(false);
         }
     }, [])
+    const deleteQuote = async (id) => {
+        try {
+            setIsLoading(true);
+            const response = await http.delete(`/quotes/${id}`);
+            if (response.status === 200) {
+                fetchQuotes();
+            }
+        } catch (error) {
+            console.error("Gagal menghapus quote:", error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
 
     useEffect(() => {
         fetchQuotes()
@@ -27,8 +42,14 @@ export default function QuotePage() {
         return <div>Loading...</div>
     } else {
         return <div className="container mx-auto space-y-5">
-            <h1 className="font-semibold text-2xl">Quotes</h1>
-            <Button />
+            <div className="flex justify-between items-center mb-4">
+                <h1 className="font-semibold text-2xl">Quotes</h1>
+                <div className="px-5 py-5">
+                <NavLink to="/new-quote">
+                    <Button>Create New Quote</Button>
+                </NavLink>
+                </div>
+            </div>
             <ul className="space-y-4 divide-y divide-zinc-200 dark:divide-zinc-700">
                 {quotes.map((quote) => (
                     <li key={quote.id} className="pt-4 p-5 border border-slate-300">
@@ -45,6 +66,17 @@ export default function QuotePage() {
                                 {quote.category}
                             </span>
                         )}
+                        <div className="flex gap-3 flex-row justify-end">
+                        <div className="px5 py-2">
+                            <Button onClick={() => deleteQuote(quote.id)}>Hapus</Button>
+                        </div>
+                        <div className="px5 py-2">
+                            <NavLink to={`/update-quote/${quote.id}`}>
+                                <Button>Update Quote</Button>
+                            </NavLink>
+                        </div>
+
+                        </div>
                     </li>
                 ))}
             </ul>
